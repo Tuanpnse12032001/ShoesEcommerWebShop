40194569fe2567564771f94c18a755b12d0dc9d7
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-04 11:05:35 +0700

    push

---
+++ src/main/java/org/example/shoesecommerwebshop/ShoesEcommerWebShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoesecommerwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesEcommerWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesEcommerWebShopApplication.class, args);
+	}
+
+}
