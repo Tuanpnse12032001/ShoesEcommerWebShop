ccc8ef0622cdbcd3878e9183e5a5a3ac3aa7e27c
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-23 19:08:11 +0700

    class payload and model

---
+++ src/main/java/domain/payload/LoginResponse.java
@@ -0,0 +1,13 @@
+package domain.payload;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
